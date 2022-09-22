ec735c9ff76bcb0e0263fdb9c8ea286abb32bd40
imohsinali <mohsinbalty@gmail.com>
2022-09-22 23:01:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
-
+import { useState } from 'react'
+import Select from './Select'
 function App() {
+const options=[
+  {label:"First",value:1},
+  {label:"2nd",value:2},
+  {label:"3rd",value:3},
+  {label:"4th",value:4},
+  {label:"5th",value:5}
+  
 
+]
+
+const [value,setValue]=useState<typeof options[0] | undefined>(options[0])
   return (
     <div className="App">
-      Hello
+    <Select options={options} value={value} onChange={o=>setValue(o)}/>
     </div>
   )
 }
