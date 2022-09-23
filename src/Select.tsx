8c7cee8c58cbb162d3d34c95459b8dbef6dc9bb4
imohsinali <mohsinbalty@gmail.com>
2022-09-23 19:02:11 +0500

    Update Select.tsx

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,62 +1,96 @@
 import { useEffect, useState } from 'react'
 import style from './select.module.css'
-type SelectOptions={
-    label:String
-    value:string|number
+type SelectOptions = {
+  label: String
+  value: string | number
+}
+type MultipleSelectProps = {
+  multiple: true
+  value: SelectOptions[]
+  onChange: (value: SelectOptions[]) => void
+
+
 }
 
-type SelectProps={
-    options:SelectOptions[]
-    value?:SelectOptions
-    onChange:(value:SelectOptions | undefined)=>void
+type SingleSelectProps = {
+  multiple?: false
+  value?: SelectOptions
+  onChange: (value: SelectOptions | undefined) => void
 
 }
-const Select = ({value,onChange,options}:SelectProps) => {
+type SelectProps = {
+  options: SelectOptions[]
+
+
+}
+  & (SingleSelectProps | MultipleSelectProps)
+const Select = ({ multiple, value, onChange, options }: SelectProps) => {
   console.warn(value)
-  const [isOpen,setIsOpen]=useState(false)
-  const [highlightIndex,setHighlightIndex]=useState(0)
-  const selectOption=(option:SelectOptions)=>{
+  const [isOpen, setIsOpen] = useState(false)
+  const [highlightIndex, setHighlightIndex] = useState(0)
+  const selectOption = (option: SelectOptions) => {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter(o => o !== option))
+      }
+      else {
+        onChange([...value, option])
 
-   if(option!==value)  onChange(option)
+      }
+    }
+    else {
+      if (option !== value) onChange(option)
+    }
+  }
+  const isOptionSelected = (option: SelectOptions) => {
+    return multiple ? value.includes(option) : option.label == value?.label
 
   }
-  const isOptionSelected=(option:SelectOptions)=>{
-    return option.label==value?.label
-
+  const clearOptions = () => {
+    multiple ? onChange([]) : onChange(undefined)
   }
   useEffect(() => {
-    if(isOpen) setHighlightIndex(0)
+    if (isOpen) setHighlightIndex(0)
   }, [isOpen]);
   return (
     <>
-    <div tabIndex={0} className={style.container} 
-    onClick={()=>setIsOpen(p=>!p)}
-    onBlur={()=>setIsOpen(false)}>
-      <span className={style.value}>{value?.label}</span>
-      <button className={style['clear-btn']}
-      onClick={(e)=>{onChange(undefined)
-        e.stopPropagation()}  }>&times;</button
-      >
-      <div className={style.divider}></div>
-      <div className={style.caret}>&#9660;</div>
-      <ul className={`${style.options} ${ isOpen?style.show:""}`}>
-        {
-          options.map((option,index)=>(
-            <li
-            onMouseEnter={()=>setHighlightIndex(index)}
-            key={option.value} className={`${style.option} ${isOptionSelected(option)?style.selected:''} 
-            ${index==highlightIndex?style.highlighted:''}`}
-            
+      <div tabIndex={0} className={style.container}
+        onClick={() => setIsOpen(p => !p)}
+        onBlur={() => setIsOpen(false)}>
+        <span className={style.value}>{multiple ? value.map(v => (
+          <button key={v.value} onClick={e => {
+            e.stopPropagation()
+            selectOption(v)
+          }}
+            className={style['option-badge']}
+          >{v.label}
+            <span className={style['remove-bnt']}>&times;</span>
+          </button>
+        )) : value?.label}</span>
+        <button className={style['clear-btn']}
+          onClick={clearOptions}>&times;</button>
+        <div className={style.divider}></div>
+        <div className={style.caret}>&#9660;</div>
+        <ul className={`${style.options} ${isOpen ? style.show : ""}`}>
+          {
+            options.map((option, index) => (
+              <li
+                onMouseEnter={() => setHighlightIndex(index)}
+                key={option.value} className={`${style.option} ${isOptionSelected(option) ? style.selected : ''} 
+            ${index == highlightIndex ? style.highlighted : ''}`}
 
-            onClick={(e)=>{e.stopPropagation()
-            selectOption(option)
-            setIsOpen(false)}}>
-                  {option.label}
-            </li> 
-          ))
-        }
-      </ul>
-    </div>
+
+                onClick={(e) => {
+                  e.stopPropagation()
+                  selectOption(option)
+                  setIsOpen(false)
+                }}>
+                {option.label}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </>
   )
 }
