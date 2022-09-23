1d36e217ebf649766e3eb27ca511a07cea11c7e4
imohsinali <mohsinbalty@gmail.com>
2022-09-23 19:01:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
-import Select from './Select'
+import Select, { SelectOptions } from './Select'
 function App() {
-const options=[
-  {label:"First",value:1},
-  {label:"2nd",value:2},
-  {label:"3rd",value:3},
-  {label:"4th",value:4},
-  {label:"5th",value:5}
-  
+  const options = [
+    { label: "First", value: 1 },
+    { label: "2nd", value: 2 },
+    { label: "3rd", value: 3 },
+    { label: "4th", value: 4 },
+    { label: "5th", value: 5 }
 
-]
 
-const [value,setValue]=useState<typeof options[0] | undefined>(options[0])
+  ]
+
+  const [value1, setValue1] = useState<SelectOptions[]>([options[0]])
+  const [value2, setValue2] = useState<SelectOptions | undefined>(options[0])
+
   return (
     <div className="App">
-    <Select options={options} value={value} onChange={o=>setValue(o)}/>
+      <Select multiple options={options} value={value1} onChange={o => setValue1(o)} />
+      <br />
+      <Select options={options} value={value2} onChange={o => setValue2(o)} />
+
     </div>
   )
 }
